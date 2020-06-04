f860bf521b73a079d328377a5b35e6395c64012e
playoutprod <35104962+playoutprod@users.noreply.github.com>
2020-06-04 12:26:35 +0200

    Add preview :)

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 React Tic Tac Toe, Google Material Design Light style.
 
+![preview](public/preview.png)
+
 [Result here](https://3wa-ex-tictactoe-hlbizylpb.now.sh/)
+
 
 - Level 1 : useReducer
 - Level 2 : propTypes
--- src/components/Board.js
+++ src/components/Board.js
@@ -6,7 +6,7 @@ import Message from "./Message";
 
 import ThemeContext from '../config/themes.js';
 
-const winPositions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6],[0,4,8]]
+const winPositions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6],[0,4,8]];
 
 const Board = (props) => {
 
